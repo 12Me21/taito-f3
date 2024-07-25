8c9fc99aab272b3d6576621735373256f18f1f0e
12Me21 <12Me21.mc@gmail.com>
2024-07-24 22:44:21 -0400

    image pixel size adjust

--- website/link.js
+++ website/link.js
@@ -13,3 +13,22 @@ for (var i=0; i<h.length; i++) {
 	let t = event.target
 	if (t.id) location.hash = t.id
 }*/
+
+//egh..
+for (let img of document.images) {
+	img.style.width = "calc("+img.getAttribute('width')+"px * var(--RX,1) / var(--X,1))"
+	img.style.height = "calc("+img.getAttribute('height')+"px * var(--RX,1) / var(--X,1))"
+}
+
+var ql
+~function self() {
+	var dpr = +window.devicePixelRatio
+	var s = document.documentElement.style
+	let adpr = dpr > 0.75 ? dpr : 1
+	s.setProperty('--X', adpr || 1) // device pixels per css pixel
+	s.setProperty('--RX', Math.round(adpr) || 1) // rounded
+	// refresh listener
+	ql && ql.removeListener(self)
+	ql = matchMedia("(-webkit-device-pixel-ratio: "+dpr+")")
+	ql.addListener(self)
+}()
