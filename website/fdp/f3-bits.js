87fcfd0b24b6ab4a232ba4b445ca7e87c61e1353
12Me21 <12Me21.mc@gmail.com>
2025-11-12 16:58:35 -0500

    trying things test2

--- website/fdp/f3-bits.js
+++ website/fdp/f3-bits.js
@@ -6,16 +6,23 @@ function defer(fn) {
 defer(e=>{
 	for (let e of document.querySelectorAll('f3-bits')) {
 		if (/,|:/.test(e.textContent)) {
-			let n = e.innerHTML.split(",")
+			let n = e.innerHTML.split(",").reverse()
 			let l = 0
+			let nums = []
+			let hides = []
 			let d = n.map(m=>{
 				m = m.split(":")
-				console.log(m)
 				let c = (+m[1])||1
+				nums.push(l)
+				if (m[0]=="#") {
+					for (let i=0;i<c;i++)
+						hides.push(l+i)
+					m[0]="."
+				}
 				l += c
 				return `<td${/^(⋯|-|0|1|\.)$/.test(m[0])?" class=x":""} colspan=${c}>${m[0]}`
 			})
-			e.outerHTML = `<table class=bits><tr style="counter-reset:bnum ${l}">${"<th>".repeat(l)}<tr>${d.join("")}</table>`
+			e.outerHTML = `<table class=bits><tr>${new Array(l).fill().map((x,i)=>hides.includes(i) ? "<th class=x>" : (nums.includes(i) ? "<th class=f>"+(i) : "<th>"+i)).reverse().join("")}<tr>${d.reverse().join("")}</table>`
 		} else {
 			let n = e.textContent.replace(/ /g,"")
 			e.outerHTML = `<table class=bits><tr style="counter-reset:bnum ${n.length}">${"<th>".repeat(n.length)}<tr>${n.replace(/[01]|([^01])( ?\1)*/g, (m)=>`<td${m[0]<="1"?" class=x":""} colspan=${m.length}>${m[0]}`)}</table>`
