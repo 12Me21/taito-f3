56ef9556980b850ffca340bde38004dfb3413bbe
12Me21 <12Me21.mc@gmail.com>
2025-11-12 23:59:25 -0500

    tilemap page, many changes, vertical scroll etc

--- website/fdp/f3-bits.js
+++ website/fdp/f3-bits.js
@@ -6,23 +6,37 @@ function defer(fn) {
 defer(e=>{
 	for (let e of document.querySelectorAll('f3-bits')) {
 		if (/,|:/.test(e.textContent)) {
-			let n = e.innerHTML.split(",").reverse()
-			let l = 0
-			let nums = []
-			let hides = []
-			let d = n.map(m=>{
-				m = m.split(":")
-				let c = (+m[1])||1
-				nums.push(l)
-				if (m[0]=="#") {
-					for (let i=0;i<c;i++)
-						hides.push(l+i)
-					m[0]="."
+			let th = []
+			let td = []
+			let offs = 0
+			for (let def of e.innerHTML.split(",").reverse()) {
+				let [name, len] = def.split(":")
+				len = +len || 1
+				let hide = /^(#)$/.test(name)
+				let special = /^(⋯|-|0|1|[.])$/.test(name)
+				let cont = /^(⋯)$/.test(name)
+				
+				for (let i=0; i<len; i++) {
+					if (hide) {
+						th.push(`<th>`)
+					} else {
+						let strong = i==0 && !cont
+						th.push((strong ? `<th class=f>` : `<th>`) + (offs+i))
+					}
 				}
-				l += c
-				return `<td${/^(⋯|-|0|1|\.)$/.test(m[0])?" class=x":""} colspan=${c}>${m[0]}`
-			})
-			e.outerHTML = `<table class=bits><tr>${new Array(l).fill().map((x,i)=>hides.includes(i) ? "<th class=x>" : (nums.includes(i) ? "<th class=f>"+(i) : "<th>"+i)).reverse().join("")}<tr>${d.reverse().join("")}</table>`
+				
+				let d = `<td colspan=${len}`
+				if (hide || special)
+					d += ` class=x`
+				d += `>`
+				if (!hide)
+					d += name
+				
+				td.push(d)
+				
+				offs += len
+			}
+			e.outerHTML =`<table class=bits><tr>${th.reverse().join("")}<tr>${td.reverse().join("")}</table>`
 		} else {
 			let n = e.textContent.replace(/ /g,"")
 			e.outerHTML = `<table class=bits><tr style="counter-reset:bnum ${n.length}">${"<th>".repeat(n.length)}<tr>${n.replace(/[01]|([^01])( ?\1)*/g, (m)=>`<td${m[0]<="1"?" class=x":""} colspan=${m.length}>${m[0]}`)}</table>`
